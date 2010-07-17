699a77edc46a5b720336523b96cb747a1141dc8c
Ratnadeep Debnath <rtnpro@gmail.com>
2010-07-18 02:10:50 +0530

    updated wiktionary.py to show a scrapped view of wiktionary

--- wiktionary.py
+++ wiktionary.py
@@ -8,6 +8,7 @@ opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 html = opener.open(url).read()
 soup = BeautifulSoup(html)
 
+"""
 for h3 in soup.html.body.findAll('h3'):
     if h3.findAll(id='Etymology'):
         tmp_ety = h3.findNext('p').renderContents().split('<')
@@ -20,6 +21,21 @@ for i in tmp_ety:
         t = x[len(x) - 1]
         if t.find('&#'):
             etymology = etymology + t
+"""
+#remove the floating wikipedia image and link
+for i in soup.html.body.findAll('div', {'class' : 'sister-wikipedia sister-project noprint floatright'}):
+    floating = i
+floating.findNext('p').extract()
+floating.extract()
 
-print etymology
+#extract contents only
+for i in soup.html.body.findAll('div', {'id' : 'content'}):
+    contents = i
+soup.find(href='http://bits.wikimedia.org/skins-1.5/monobook/main.css?283l').replaceWith('<link rel="stylesheet" href="main.css" type="text/css" media="screen" />')
+head = soup.html.head
+tmp = '<html>' + '\n' + str(head) + '\n' + '<body>\n' + str(contents) + '\n</body>' + '</html>'
+file = open('tmp.html', 'w')
+file.write(tmp)
+file.close()
+
 
