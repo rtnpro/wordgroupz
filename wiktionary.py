e82bdbf1a21d8e60ab3f0d32c3e70bef501f9215
Ratnadeep Debnath <rtnpro@gmail.com>
2010-07-19 17:51:20 +0530

    usage: python wiktionary.py <word>

--- wiktionary.py
+++ wiktionary.py
@@ -1,6 +1,40 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
 import sys
+import gtk
+import webkit
+
+def wikitionary_lookup(html_str):
+    window = gtk.Window()
+    window.set_default_size(500,600)
+    window.connect("destroy", on_window_destroy)
+    vbox = gtk.VBox()
+    browser = webkit.WebView()
+    scroller = gtk.ScrolledWindow()
+    scroller.add(browser)
+    vbox.pack_start(scroller)
+    progress = gtk.ProgressBar()
+    browser.connect("load-progress-changed", load_progress_changed)
+    browser.connect("load-started", load_started)
+    browser.connect("load-finished", load_finished)
+    vbox.pack_start(progress, False)
+    window.add(vbox)
+    window.show_all()
+    #browser.open('file:///home/rtnpro/webscrapping/tmp.html')
+    browser.load_html_string(html_str, 'http://en.wiktionary.org/wiki/'+sys.argv[1])
+    gtk.main()
+
+def load_progress_changed(webview, amount):
+    progress.set_fraction(amount/100.0)
+
+def load_started(webview, frame):
+    progress.set_visible(True)
+
+def load_finished(webview, frame):
+    progress.set_visible(False)
+
+def on_window_destroy(widget, data = None):
+    gtk.main_quit()
 
 url = 'http://en.wiktionary.org/wiki/' + sys.argv[1]
 opener = urllib2.build_opener()
@@ -24,18 +58,21 @@ for i in tmp_ety:
 """
 #remove the floating wikipedia image and link
 for i in soup.html.body.findAll('div', {'class' : 'sister-wikipedia sister-project noprint floatright'}):
-    floating = i
-floating.findNext('p').extract()
-floating.extract()
+    i.findNext('p').extract()
+    i.extract()
 
 #extract contents only
 for i in soup.html.body.findAll('div', {'id' : 'content'}):
     contents = i
-soup.find(href='http://bits.wikimedia.org/skins-1.5/monobook/main.css?283l').replaceWith('<link rel="stylesheet" href="main.css" type="text/css" media="screen" />')
+soup.find(href='http://bits.wikimedia.org/skins-1.5/monobook/main.css?283l').replaceWith('<link rel="stylesheet" href="http://rtnpro.fedorapeople.org/main.css" type="text/css" media="screen" />')
 head = soup.html.head
 tmp = '<html>' + '\n' + str(head) + '\n' + '<body>\n' + str(contents) + '\n</body>' + '</html>'
+
 file = open('tmp.html', 'w')
 file.write(tmp)
 file.close()
+
+
+wikitionary_lookup(tmp)
 
 
